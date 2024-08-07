1ab02e933ba5c347ec392571a013e83ed49d3975
katlex_93 <93kafiya@gmail.com>
2024-08-07 18:51:20 +0200

    Time Travel

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ export function Square({value, onSquareClick}) {
   
 }
 
-export default function Board() { 
-  const [xIsNext, setXIsNext] =useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
+function Board({xIsNext, squares, onPlay}) { 
+  
 
   function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)){
+    if (calculateWinner(squares)|| squares[i]){
       return;
     }
     const nextSquares = squares.slice();
@@ -27,8 +26,7 @@ export default function Board() {
     else {
       nextSquares[i]= "O"
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares);
@@ -39,6 +37,11 @@ export default function Board() {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
   
+  
+  
+
+
+
   return (
   <>
   <div className="status">{status}</div>
@@ -64,22 +67,44 @@ export default function Board() {
   </> );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
-  return null;
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
